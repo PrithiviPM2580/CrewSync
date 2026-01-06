440c273fcbf4505943c05e07b8efe691032fb8d8
Prithivi <prithivi@users.noreply.github.com>
2026-01-06 21:26:59 +0545

    Add User model with schema and password hashing functionality

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,71 @@
+import mongoose, { Document, mongo, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+export interface UserDocument extends Document {
+  name: string;
+  email: string;
+  password?: string;
+  profilePicture: string | null;
+  isActive: boolean;
+  lastLogin: Date | null;
+  createdAt: Date;
+  updatedAt: Date;
+  currentWorkspace: mongoose.Types.ObjectId | null;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<UserDocument>(
+  {
+    name: {
+      type: String,
+      required: false,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      select: false,
+    },
+    profilePicture: {
+      type: String,
+      default: null,
+    },
+    currentWorkspace: {
+      type: mongoose.Types.ObjectId,
+      ref: "Workspace",
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre<UserDocument>("save", async function () {
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password!, salt);
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  return bcrypt.compare(candidatePassword, this.password!);
+};
+
+const User = mongoose.model<UserDocument>("User", userSchema);
+
+export default User;
