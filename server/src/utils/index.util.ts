c1c195161e3f2e741e437b214eb9dd22caeedd19
Prithivi <prithivi@users.noreply.github.com>
2026-01-06 22:34:16 +0545

    Add RolePermissions mapping for user roles and their permissions

--- server/src/utils/index.util.ts
+++ server/src/utils/index.util.ts
@@ -1,6 +1,11 @@
 import type { Response } from "express";
 import { z } from "zod";
 import { v4 as uuidv4 } from "uuid";
+import {
+  PermissionEnum,
+  type PermissionEnumType,
+  type RoleEnumType,
+} from "@/enums/index.enum.js";
 
 export function formattedIssues(issues: z.ZodError["issues"]) {
   return issues.map((issue) => ({
@@ -30,3 +35,41 @@ export function generateInviteCode(): string {
 export function generateTaskCode(): string {
   return `task-${uuidv4().replace(/-/g, "").substring(0, 3)}`;
 }
+
+export const RolePermissions: Record<
+  RoleEnumType,
+  Array<PermissionEnumType>
+> = {
+  OWNER: [
+    PermissionEnum.CREATE_WORKSPACE,
+    PermissionEnum.EDIT_WORKSPACE,
+    PermissionEnum.DELETE_WORKSPACE,
+    PermissionEnum.MANAGE_WORKSPACE_SETTINGS,
+    PermissionEnum.ADD_MEMBER,
+    PermissionEnum.CHANGE_MEMBER_ROLE,
+    PermissionEnum.REMOVE_MEMBER,
+    PermissionEnum.CREATE_PROJECT,
+    PermissionEnum.EDIT_PROJECT,
+    PermissionEnum.DELETE_PROJECT,
+    PermissionEnum.CREATE_TASK,
+    PermissionEnum.EDIT_TASK,
+    PermissionEnum.DELETE_TASK,
+    PermissionEnum.VIEW_ONLY,
+  ],
+  ADMIN: [
+    PermissionEnum.ADD_MEMBER,
+    PermissionEnum.CREATE_PROJECT,
+    PermissionEnum.EDIT_PROJECT,
+    PermissionEnum.DELETE_PROJECT,
+    PermissionEnum.CREATE_TASK,
+    PermissionEnum.EDIT_TASK,
+    PermissionEnum.DELETE_TASK,
+    PermissionEnum.VIEW_ONLY,
+    PermissionEnum.MANAGE_WORKSPACE_SETTINGS,
+  ],
+  MEMBER: [
+    PermissionEnum.CREATE_TASK,
+    PermissionEnum.EDIT_TASK,
+    PermissionEnum.VIEW_ONLY,
+  ],
+};
