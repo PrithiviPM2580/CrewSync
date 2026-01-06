81e6c8c24410346702b09923bd442e90aa2f8380
Prithivi <prithivi@users.noreply.github.com>
2026-01-06 16:49:44 +0545

    Add environment configuration file with validation using Zod

---
+++ server/src/config/env.config.ts
@@ -0,0 +1,19 @@
+import validateSchema from "@/lib/validate.lib.js";
+import { envSchema } from "@/validator/env.validator.js";
+
+const envConfig = {
+  PORT: process.env.PORT,
+  NODE_ENV: process.env.NODE_ENV,
+  MONGODB_URI: process.env.MONGODB_URI,
+  SESSION_SECRET: process.env.SESSION_SECRET,
+  SESSION_EXPIRY: process.env.SESSION_EXPIRY,
+  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+  FRONTEND_ORIGIN: process.env.FRONTEND_ORIGIN,
+  FRONTEND_GOOGLE_CALLBACK_URL: process.env.FRONTEND_GOOGLE_CALLBACK_URL,
+};
+
+const config = validateSchema(envSchema, envConfig);
+
+export default config;
