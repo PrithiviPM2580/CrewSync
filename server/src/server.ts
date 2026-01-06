61398242eefb30b91bc9db76f83deacb951980e5
Prithivi <prithivi@users.noreply.github.com>
2026-01-06 19:19:30 +0545

    Make startServer function asynchronous and add database connection logic

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,12 @@
 import app from "@/app.js";
 import config from "@/config/env.config.js";
+import connectToDatabase from "./config/db.config.js";
 
 const PORT = config.PORT || 3000;
 
-export default function startServer() {
+export default async function startServer() {
   try {
+    await connectToDatabase();
     app.listen(PORT, () => {
       console.log(`Server is running in http://localhost:${PORT}`);
     });
