bc4ed06d79f2422dae1ddfd9516ca60cac059110
Prithivi <prithivi@users.noreply.github.com>
2026-01-08 19:25:20 +0545

    Refactor authentication schemas to include body structure for validation

--- server/src/validator/auth.validator.ts
+++ server/src/validator/auth.validator.ts
@@ -1,34 +1,38 @@
 import { z } from "zod";
 
-export const registerSchema = z.object({
-  name: z
-    .string()
-    .min(2, "Name must be at least 2 characters long")
-    .trim()
-    .min(1, "Name is required")
-    .max(100, "Name must be at most 100 characters long"),
-  email: z
-    .email("Invalid email address")
-    .toLowerCase()
-    .trim()
-    .min(1, "Email is required"),
-  password: z
-    .string()
-    .min(6, "Password must be at least 6 characters long")
-    .max(100, "Password must be at most 100 characters long"),
-});
+export const registerSchema = {
+  body: z.object({
+    name: z
+      .string()
+      .min(2, "Name must be at least 2 characters long")
+      .trim()
+      .min(1, "Name is required")
+      .max(100, "Name must be at most 100 characters long"),
+    email: z
+      .email("Invalid email address")
+      .toLowerCase()
+      .trim()
+      .min(1, "Email is required"),
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters long")
+      .max(100, "Password must be at most 100 characters long"),
+  }),
+};
 
-export const loginSchema = z.object({
-  email: z
-    .email("Invalid email address")
-    .toLowerCase()
-    .trim()
-    .min(1, "Email is required"),
-  password: z
-    .string()
-    .min(6, "Password must be at least 6 characters long")
-    .max(100, "Password must be at most 100 characters long"),
-});
+export const loginSchema = {
+  body: z.object({
+    email: z
+      .email("Invalid email address")
+      .toLowerCase()
+      .trim()
+      .min(1, "Email is required"),
+    password: z
+      .string()
+      .min(6, "Password must be at least 6 characters long")
+      .max(100, "Password must be at most 100 characters long"),
+  }),
+};
 
 export type RegisterType = z.infer<typeof registerSchema>;
 export type LoginType = z.infer<typeof loginSchema>;
