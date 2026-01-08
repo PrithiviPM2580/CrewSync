85a766c255a1eb4e81dbebcb35c8c2e16ff18743
Prithivi <prithivi@users.noreply.github.com>
2026-01-08 22:51:55 +0545

    Add current user route with API rate limiting and async error handling

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -1,7 +1,10 @@
+import asyncHandler from "@/middlewares/async-handler.middleware.js";
+import { apiLimitter } from "@/middlewares/rate-limiter.middleware.js";
 import { Router } from "express";
+import { currentUserController } from "@/controllers/user.controller.js";
 
 const router: Router = Router();
 
-router.route("/current").get();
+router.route("/current").get(apiLimitter, asyncHandler(currentUserController));
 
 export default router;
