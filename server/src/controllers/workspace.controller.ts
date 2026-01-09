4191eb337985a4eb8b7268272fd3fa916478ee6f
Prithivi <prithivi@users.noreply.github.com>
2026-01-09 18:40:29 +0545

    Add controller for retrieving all workspaces user is a member of

--- server/src/controllers/workspace.controller.ts
+++ server/src/controllers/workspace.controller.ts
@@ -2,7 +2,10 @@ import { APIError } from "@/lib/error-handler.lib.js";
 import logger from "@/lib/logger.lib.js";
 import { successResponse } from "@/utils/index.util.js";
 import type { NextFunction, Request, Response } from "express";
-import { createWorkspaceService } from "@/services/workspace.service.js";
+import {
+  createWorkspaceService,
+  getAllWorkspacesUserIsMember,
+} from "@/services/workspace.service.js";
 
 export async function createWorkspaceController(
   req: Request,
@@ -25,6 +28,31 @@ export async function createWorkspaceController(
   });
 
   successResponse(res, 201, "Workspace created successfully", {
+    workspace,
+  });
+}
+
+export async function getAllWorkspacesUserIsMemberController(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const userId = req.user?._id;
+
+  if (!userId) {
+    logger.error("Unauthorized access attempt to get workspaces", {
+      label: "WorkspaceController",
+    });
+    return next(new APIError(401, "Unauthorized, user not authenticated"));
+  }
+
+  const { workspace } = await getAllWorkspacesUserIsMember(userId);
+
+  logger.info(`Fetched all workspaces for user ${userId}`, {
+    label: "WorkspaceController",
+  });
+
+  successResponse(res, 200, "Workspaces fetched successfully", {
     workspace,
   });
 }
