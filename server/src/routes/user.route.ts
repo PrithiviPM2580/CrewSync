1c86202d432f0b6527726a59d6d67f4024d5d82d
Prithivi <prithivi@users.noreply.github.com>
2026-01-09 08:41:05 +0545

    Add authentication check to current user route for enhanced security

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -2,9 +2,12 @@ import asyncHandler from "@/middlewares/async-handler.middleware.js";
 import { apiLimitter } from "@/middlewares/rate-limiter.middleware.js";
 import { Router } from "express";
 import { currentUserController } from "@/controllers/user.controller.js";
+import { isAuthenticated } from "@/middlewares/isAunticates.middleware.js";
 
 const router: Router = Router();
 
-router.route("/current").get(apiLimitter, asyncHandler(currentUserController));
+router
+  .route("/current")
+  .get(apiLimitter, isAuthenticated, asyncHandler(currentUserController));
 
 export default router;
