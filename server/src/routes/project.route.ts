668c7f72aaecd43fbdd3690ce7f65ba86e12d9ca
Prithivi <prithivi@users.noreply.github.com>
2026-01-10 17:51:57 +0545

    Add getAllprojects route and controller for fetching all projects

--- server/src/routes/project.route.ts
+++ server/src/routes/project.route.ts
@@ -1,9 +1,15 @@
-import { createProjectController } from "@/controllers/project.controller.js";
+import {
+  createProjectController,
+  getAllprojectsController,
+} from "@/controllers/project.controller.js";
 import asyncHandler from "@/middlewares/async-handler.middleware.js";
 import { isAuthenticated } from "@/middlewares/isAunticates.middleware.js";
 import { apiLimitter } from "@/middlewares/rate-limiter.middleware.js";
 import validateRequestMiddleware from "@/middlewares/request-validate.middleware.js";
-import { createProjectSchema } from "@/validator/project.validator.js";
+import {
+  createProjectSchema,
+  getAllprojectSchema,
+} from "@/validator/project.validator.js";
 import { Router } from "express";
 
 const router: Router = Router();
@@ -15,6 +21,15 @@ router
     isAuthenticated,
     validateRequestMiddleware(createProjectSchema),
     asyncHandler(createProjectController)
+  );
+
+router
+  .route("/workspace/:workspaceId/all")
+  .get(
+    apiLimitter,
+    isAuthenticated,
+    validateRequestMiddleware(getAllprojectSchema),
+    asyncHandler(getAllprojectsController)
   );
 
 export default router;
