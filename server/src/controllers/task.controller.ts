11abc4e363be23ef9f75e04484c811b9b15f208d
Prithivi <prithivi@users.noreply.github.com>
2026-01-11 00:04:03 +0545

    Add updateTask controller with authentication and role validation

--- server/src/controllers/task.controller.ts
+++ server/src/controllers/task.controller.ts
@@ -4,7 +4,10 @@ import logger from "@/lib/logger.lib.js";
 import { getMemberRoleInWorkspace } from "@/services/member.service.js";
 import { roleGuard, successResponse } from "@/utils/index.util.js";
 import type { NextFunction, Request, Response } from "express";
-import { createTaskService } from "@/services/task.service.js";
+import {
+  createTaskService,
+  updateTaskService,
+} from "@/services/task.service.js";
 
 export async function createTaskController(
   req: Request,
@@ -44,4 +47,45 @@ export async function createTaskController(
   });
 
   successResponse(res, 201, "Task created successfully", { task });
+}
+
+export async function updateTaskController(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const body = req.body;
+  const projectId = req.params.projectId!;
+  const workspaceId = req.params.workspaceId!;
+  const taskId = req.params.id!;
+
+  const userId = req.user?._id;
+
+  if (!userId) {
+    logger.error("User not authenticated", {
+      label: "UpdateTaskController",
+    });
+
+    return next(new APIError(401, "User not authenticated"));
+  }
+
+  const { role } = await getMemberRoleInWorkspace(userId, workspaceId);
+  roleGuard(role, [PermissionEnum.EDIT_TASK]);
+
+  const { updatedTask } = await updateTaskService(
+    workspaceId,
+    projectId,
+    taskId,
+    body
+  );
+
+  logger.info("Task updated successfully", {
+    label: "UpdateTaskController",
+    taskId: updatedTask._id,
+    workspaceId,
+    projectId,
+    userId,
+  });
+
+  successResponse(res, 200, "Task updated successfully", { updatedTask });
 }
