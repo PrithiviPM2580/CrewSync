3e9cd9eda92754b8a18304fbb3fdea6d93edf851
Prithivi <prithivi@users.noreply.github.com>
2026-01-11 00:04:09 +0545

    Add updateTaskService with validation for project and task existence

--- server/src/services/task.service.ts
+++ server/src/services/task.service.ts
@@ -4,14 +4,17 @@ import logger from "@/lib/logger.lib.js";
 import Member from "@/models/member.model.js";
 import Project from "@/models/project.model.js";
 import Task from "@/models/task.model.js";
-import type { CreateTaskType } from "@/validator/task.validator.js";
+import type {
+  CreateTaskBodyType,
+  UpdateTaskBodyType,
+} from "@/validator/task.validator.js";
 import type { Types } from "mongoose";
 
 export async function createTaskService(
   workspaceId: string,
   projectId: string,
   userId: Types.ObjectId,
-  body: CreateTaskType
+  body: CreateTaskBodyType
 ) {
   const { title, description, priority, status, assignedTo, dueDate } = body;
 
@@ -55,4 +58,54 @@ export async function createTaskService(
   await task.save();
 
   return { task };
+}
+
+export async function updateTaskService(
+  workspaceId: string,
+  projectId: string,
+  taskId: string,
+  body: UpdateTaskBodyType
+) {
+  const projects = await Project.findById(projectId);
+
+  if (!projects || projects.workspace.toString() !== workspaceId) {
+    logger.error("Project not found or does not belong to the workspace", {
+      label: "UpdateTaskService",
+      projectId,
+      workspaceId,
+    });
+    throw new APIError(
+      404,
+      "Project not found or does not belong to the workspace"
+    );
+  }
+
+  const task = await Task.findById(taskId);
+
+  if (!task || task.project.toString() !== projectId.toString()) {
+    logger.error("Task not found or does not belong to the project", {
+      label: "UpdateTaskService",
+      taskId,
+      projectId,
+    });
+    throw new APIError(404, "Task not found or does not belong to the project");
+  }
+
+  const updatedTask = await Task.findByIdAndUpdate(
+    taskId,
+    {
+      ...body,
+    },
+    { new: true }
+  );
+
+  if (!updatedTask) {
+    logger.error("Failed to update task", {
+      label: "UpdateTaskService",
+      taskId,
+    });
+    throw new APIError(500, "Failed to update task");
+  }
+
+  return { updatedTask };
 }
