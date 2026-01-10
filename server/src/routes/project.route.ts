07525ed85589ba25b362a9ac5de32387c26de2c2
Prithivi <prithivi@users.noreply.github.com>
2026-01-10 16:04:59 +0545

    Add createProject route to handle project creation requests

--- server/src/routes/project.route.ts
+++ server/src/routes/project.route.ts
@@ -1,5 +1,20 @@
+import { createProjectController } from "@/controllers/project.controller.js";
+import asyncHandler from "@/middlewares/async-handler.middleware.js";
+import { isAuthenticated } from "@/middlewares/isAunticates.middleware.js";
+import { apiLimitter } from "@/middlewares/rate-limiter.middleware.js";
+import validateRequestMiddleware from "@/middlewares/request-validate.middleware.js";
+import { createProjectSchema } from "@/validator/project.validator.js";
 import { Router } from "express";
 
 const router: Router = Router();
+
+router
+  .route("/workspace/:workspaceId/create")
+  .post(
+    apiLimitter,
+    isAuthenticated,
+    validateRequestMiddleware(createProjectSchema),
+    asyncHandler(createProjectController)
+  );
 
 export default router;
