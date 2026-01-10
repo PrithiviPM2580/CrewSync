91fe372fd0a0b94896027b4fd325766c47d4405a
Prithivi <prithivi@users.noreply.github.com>
2026-01-10 18:30:32 +0545

    Add service for fetching project by ID and workspace ID with error handling

--- server/src/services/project.service.ts
+++ server/src/services/project.service.ts
@@ -1,6 +1,8 @@
 import type { CreateProjectBodyType } from "@/validator/project.validator.js";
 import Project from "@/models/project.model.js";
 import type { Types } from "mongoose";
+import logger from "@/lib/logger.lib.js";
+import { APIError } from "@/lib/error-handler.lib.js";
 
 export async function createProjectService(
   userId: Types.ObjectId,
@@ -51,4 +53,23 @@ export async function getAllProjectsService(
     totalPages,
     skip,
   };
+}
+
+export async function getProjectByIdAndWorkspaceIdService(
+  projectId: string,
+  workspaceId: string
+) {
+  const project = await Project.findOne({
+    _id: projectId,
+    workspace: workspaceId,
+  }).select("_id emoji name description");
+
+  if (!project) {
+    logger.error(`Project ${projectId} not found in workspace ${workspaceId}`, {
+      label: "ProjectService",
+    });
+    throw new APIError(404, "Project not found");
+  }
+
+  return { project };
 }
