662698606df7476c24b923b7075d2e998d49948c
Prithivi <prithivi@users.noreply.github.com>
2026-01-10 23:41:06 +0545

    Add createTaskService for task creation with project and member validation

---
+++ server/src/services/task.service.ts
@@ -0,0 +1,58 @@
+import { TaskPriorityEnum, TaskStatusEnum } from "@/enums/index.enum.js";
+import { APIError } from "@/lib/error-handler.lib.js";
+import logger from "@/lib/logger.lib.js";
+import Member from "@/models/member.model.js";
+import Project from "@/models/project.model.js";
+import Task from "@/models/task.model.js";
+import type { CreateTaskType } from "@/validator/task.validator.js";
+import type { Types } from "mongoose";
+
+export async function createTaskService(
+  workspaceId: string,
+  projectId: string,
+  userId: Types.ObjectId,
+  body: CreateTaskType
+) {
+  const { title, description, priority, status, assignedTo, dueDate } = body;
+
+  const project = await Project.findById(projectId);
+
+  if (!project || project.workspace.toString() !== workspaceId) {
+    logger.error("Project not found or does not belong to the workspace", {
+      label: "CreateTaskService",
+      projectId,
+      workspaceId,
+    });
+    throw new APIError(
+      404,
+      "Project not found or does not belong to the workspace"
+    );
+  }
+
+  if (assignedTo) {
+    const isAssignedUsermember = await Member.exists({
+      userId: assignedTo,
+      workspaceId,
+    });
+
+    if (!isAssignedUsermember) {
+      throw new APIError(400, "Assigned user is not a member of the workspace");
+    }
+  }
+
+  const task = new Task({
+    title,
+    description,
+    priority: priority || TaskPriorityEnum.MEDIUM,
+    status: status || TaskStatusEnum.TODO,
+    assignedTo,
+    createdBy: userId,
+    workspace: workspaceId,
+    project: projectId,
+    dueDate,
+  });
+
+  await task.save();
+
+  return { task };
+}
