afdd068e4699809e09540491e8539c7c0b565e7e
Prithivi <prithivi@users.noreply.github.com>
2026-01-10 16:05:12 +0545

    Add createProjectController to handle project creation logic

---
+++ server/src/controllers/project.controller.ts
@@ -0,0 +1,35 @@
+import { PermissionEnum } from "@/enums/index.enum.js";
+import { APIError } from "@/lib/error-handler.lib.js";
+import logger from "@/lib/logger.lib.js";
+import { getMemberRoleInWorkspace } from "@/services/member.service.js";
+import { roleGuard, successResponse } from "@/utils/index.util.js";
+import type { NextFunction, Request, Response } from "express";
+import { createProjectService } from "@/services/project.service.js";
+
+export async function createProjectController(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const body = req.body;
+  const workspaceId = req.params.workspaceId!;
+  const userId = req.user?._id;
+
+  if (!userId) {
+    logger.error(`Unauthorized access to create project`, {
+      label: "ProjectController",
+    });
+    return next(new APIError(401, "Unauthorized"));
+  }
+
+  const { role } = await getMemberRoleInWorkspace(userId, workspaceId);
+  roleGuard(role, [PermissionEnum.CREATE_PROJECT]);
+
+  const {} = await createProjectService(userId, workspaceId, body);
+
+  logger.info(`Project created successfully in workspace ${workspaceId}`, {
+    label: "ProjectController",
+  });
+
+  successResponse(res, 201, "Project created successfully", {});
+}
