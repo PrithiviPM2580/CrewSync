ec654587aeba5ce5e2a610ae6ff4c9582b7d63f2
Prithivi <prithivi@users.noreply.github.com>
2026-01-10 16:05:05 +0545

    Add project validation schemas for creation, update, and ID parameter

---
+++ server/src/validator/project.validator.ts
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+import { z } from "zod";
+
+export const createProjectSchema = {
+  body: z.object({
+    emoji: z.string().trim().optional(),
+    name: z
+      .string()
+      .trim()
+      .min(3, "Project name must be at least 3 characters long")
+      .max(255, "Project name must be at most 255 characters long"),
+    description: z.string().trim().optional(),
+  }),
+};
+
+export const updateProjectschema = {
+  body: z.object({
+    emoji: z.string().trim().optional(),
+    name: z
+      .string()
+      .trim()
+      .min(3, "Project name must be at least 3 characters long")
+      .max(255, "Project name must be at most 255 characters long")
+      .optional(),
+    description: z.string().trim().optional(),
+  }),
+};
+
+export const projectIdParamSchema = {
+  params: z.object({
+    projectId: z
+      .string()
+      .refine((val) => mongoose.Types.ObjectId.isValid(val), {
+        message: "Invalid project id",
+      }),
+  }),
+};
+
+export type CreateProjectType = z.infer<typeof createProjectSchema.body>;
+export type UpdateProjectType = z.infer<typeof updateProjectschema.body>;
+export type ProjectIdParamType = z.infer<typeof projectIdParamSchema.params>;
