391da52cf9a721702abad2ea93af518054358b8d
Prithivi <prithivi@users.noreply.github.com>
2026-01-13 15:48:15 +0545

    feat: integrate QueryProvider and NuqsAdapter in main component; add route paths for authentication and protected routes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import QueryProvider from "./context/QueryProvider.tsx";
+import { NuqsAdapter } from "nuqs/adapters/react";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryProvider>
+      <NuqsAdapter>
+        <App />
+      </NuqsAdapter>
+    </QueryProvider>
+  </StrictMode>
+);
